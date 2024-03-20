47da496cf3d7dfb04ede73909f6edee027824059
VincenzoMercadante <149172586+vincenzomercadante@users.noreply.github.com>
2024-03-20 16:38:48 +0100

    Initial commit

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,6 @@
+<footer class="bg-dark text-white">
+    <div class="container py-4">
+        {{-- footer info --}}
+        &copy; {{ env('APP_AUTHOR', 'Vincenzo Mercadante') }} {{ now()->year }}
+    </div>
+</footer>
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,28 @@
+<header>
+    {{-- navbar --}}
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container">
+
+            {{-- navbar logo --}}
+            <a class="navbar-brand" href="#">Navbar</a>
+
+            {{-- responsive menu handler --}}
+            <button aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation"
+                class="navbar-toggler" data-bs-target="#navbarSupportedContent" data-bs-toggle="collapse" type="button">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            {{-- links --}}
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a aria-current="page" class="nav-link active" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Link</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
